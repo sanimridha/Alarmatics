55f745eb7226b874981dc786fc2e6920b2cf5c93
sanimridha <sanimridha307@gmail.com>
2021-05-01 22:09:36 +0600

    added home tab icon

--- Navigation/Tabs.js
+++ Navigation/Tabs.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-
+import Home from '../Screens/Home';
+import Setalarm from '../Screens/Setalarm';
+import AlarmLaunching from '../Screens/AlarmLaunching';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 const Tab = createBottomTabNavigator();
 
 const Tabs = () => {
   return (
-    <View>
-      <Text>Tabs here</Text>
-    </View>
+    <Tab.Navigator>
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{
+          tabBarIcon: ({focused}) => (
+            <View>
+              <Icon name="access-alarm" size={24} color="black" />
+            </View>
+          ),
+        }}
+      />
+      <Tab.Screen name="Settings" component={Setalarm} />
+      <Tab.Screen name="Launching" component={AlarmLaunching} />
+    </Tab.Navigator>
   );
 };
 
