381cd190cdc1b17c07d3435d3f96d83f7f7623af
sanimridha <sanimridha307@gmail.com>
2021-05-01 21:36:59 +0600

    custom formate for development

---
+++ Navigation/Tabs.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+const Tabs = () => {
+  return (
+    <View>
+      <Text>Tabs here</Text>
+    </View>
+  );
+};
+
+export default Tabs;
+
+const styles = StyleSheet.create({});
