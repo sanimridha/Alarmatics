e5efdb7d01f6e667761f5074e03a7187db4f6e61
sanimridha <sanimridha307@gmail.com>
2021-05-02 11:23:03 +0600

    bottom tab bar implemented

--- Navigation/Tabs.js
+++ Navigation/Tabs.js
@@ -1,28 +1,74 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {Dimensions, StyleSheet, Text, View} from 'react-native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import Home from '../Screens/Home';
 import Setalarm from '../Screens/Setalarm';
 import AlarmLaunching from '../Screens/AlarmLaunching';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import Plus from 'react-native-vector-icons/Entypo';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 const Tab = createBottomTabNavigator();
 
 const Tabs = () => {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      tabBarOptions={{
+        showLabel: false,
+        style: {
+          backgroundColor: '#E7915B',
+          elevation: 0,
+        },
+      }}>
       <Tab.Screen
         name="Home"
         component={Home}
         options={{
           tabBarIcon: ({focused}) => (
-            <View>
-              <Icon name="access-alarm" size={24} color="black" />
+            <View style={{justifyContent: 'center', alignItems: 'center'}}>
+              <Icon
+                name="home"
+                size={30}
+                style={{color: focused ? 'white' : '#CFD6DE'}}
+              />
+              <Text style={{color: focused ? 'white' : '#CFD6DE'}}>Home</Text>
             </View>
           ),
         }}
       />
-      <Tab.Screen name="Settings" component={Setalarm} />
-      <Tab.Screen name="Launching" component={AlarmLaunching} />
+      <Tab.Screen
+        name="Settings"
+        component={Setalarm}
+        options={{
+          tabBarIcon: ({focused}) => (
+            <View style={{justifyContent: 'center', alignItems: 'center'}}>
+              <Icon
+                name="access-alarm"
+                size={30}
+                style={{color: focused ? 'white' : '#CFD6DE'}}
+              />
+              <Text style={{color: focused ? 'white' : '#CFD6DE'}}>
+                Set Alarm
+              </Text>
+            </View>
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Launching"
+        component={AlarmLaunching}
+        options={{
+          tabBarIcon: ({focused}) => (
+            <View style={{justifyContent: 'center', alignItems: 'center'}}>
+              <MaterialCommunityIcons
+                name="alarm-light-outline"
+                size={30}
+                style={{color: focused ? 'white' : '#CFD6DE'}}
+              />
+              <Text style={{color: focused ? 'white' : '#CFD6DE'}}>View</Text>
+            </View>
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 };
