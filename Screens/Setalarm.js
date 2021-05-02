20e05d841e336c41772e5c4cdcab5dfeab0e2411
sanimridha <sanimridha307@gmail.com>
2021-05-02 21:54:34 +0600

    render settings

--- Screens/Setalarm.js
+++ Screens/Setalarm.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import Feather from 'react-native-vector-icons/Feather';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const Setalarm = ({navigation}) => {
   const renderHeader = () => {
@@ -192,16 +193,18 @@ const Setalarm = ({navigation}) => {
   const renderSettings = () => {
     return (
       <View style={{}}>
-        <View
+        <TouchableOpacity
+          activeOpacity={0.9}
           style={{
             height: 70,
             width: '95%',
             backgroundColor: 'white',
             borderRadius: 10,
             alignSelf: 'center',
+            justifyContent: 'center',
             marginTop: 10,
             // _________SHADOW
-            shadowColor: 'red',
+            shadowColor: '#000',
             shadowOffset: {
               width: 2,
               height: 2,
@@ -210,8 +213,184 @@ const Setalarm = ({navigation}) => {
             shadowRadius: 3.84,
             elevation: 3,
           }}>
-          <View></View>
-        </View>
+          <View
+            style={{
+              justifyContent: 'space-between',
+              flexDirection: 'row',
+              paddingRight: 15,
+              alignItems: 'center',
+            }}>
+            <View style={{paddingLeft: 20, flexDirection: 'row'}}>
+              <MaterialCommunityIcons
+                name="bell-ring-outline"
+                size={30}
+                color="#868B98"
+                style={{alignSelf: 'center'}}
+              />
+              <View style={{flexDirection: 'column'}}>
+                <Text
+                  style={{
+                    fontSize: 20,
+                    fontWeight: 'bold',
+                    color: '#868B98',
+                    paddingLeft: 10,
+                  }}>
+                  Alarm Ringtone
+                </Text>
+                <Text
+                  style={{
+                    fontSize: 13,
+                    // fontWeight: 'bold',
+                    color: '#868B98',
+                    paddingLeft: 10,
+                  }}>
+                  Go to settings
+                </Text>
+              </View>
+            </View>
+            {/* rightArrow */}
+            <View>
+              <MaterialCommunityIcons
+                name="chevron-right"
+                size={30}
+                color="#868B98"
+                // style={{alignSelf: 'center'}}
+              />
+            </View>
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity
+          activeOpacity={0.9}
+          style={{
+            height: 70,
+            width: '95%',
+            backgroundColor: 'white',
+            borderRadius: 10,
+            alignSelf: 'center',
+            justifyContent: 'center',
+            marginTop: 10,
+            // _________SHADOW
+            shadowColor: '#000',
+            shadowOffset: {
+              width: 2,
+              height: 2,
+            },
+            shadowOpacity: 0.25,
+            shadowRadius: 3.84,
+            elevation: 3,
+          }}>
+          <View
+            style={{
+              justifyContent: 'space-between',
+              flexDirection: 'row',
+              paddingRight: 15,
+              alignItems: 'center',
+            }}>
+            <View style={{paddingLeft: 20, flexDirection: 'row'}}>
+              <MaterialCommunityIcons
+                name="bell-ring-outline"
+                size={30}
+                color="#868B98"
+                style={{alignSelf: 'center'}}
+              />
+              <View style={{flexDirection: 'column'}}>
+                <Text
+                  style={{
+                    fontSize: 20,
+                    fontWeight: 'bold',
+                    color: '#868B98',
+                    paddingLeft: 10,
+                  }}>
+                  Alarm Ringtone
+                </Text>
+                <Text
+                  style={{
+                    fontSize: 13,
+                    // fontWeight: 'bold',
+                    color: '#868B98',
+                    paddingLeft: 10,
+                  }}>
+                  Go to settings
+                </Text>
+              </View>
+            </View>
+            {/* rightArrow */}
+            <View>
+              <MaterialCommunityIcons
+                name="chevron-right"
+                size={30}
+                color="#868B98"
+                // style={{alignSelf: 'center'}}
+              />
+            </View>
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity
+          activeOpacity={0.9}
+          style={{
+            height: 70,
+            width: '95%',
+            backgroundColor: 'white',
+            borderRadius: 10,
+            alignSelf: 'center',
+            justifyContent: 'center',
+            marginTop: 10,
+            // _________SHADOW
+            shadowColor: '#000',
+            shadowOffset: {
+              width: 2,
+              height: 2,
+            },
+            shadowOpacity: 0.25,
+            shadowRadius: 3.84,
+            elevation: 3,
+          }}>
+          <View
+            style={{
+              justifyContent: 'space-between',
+              flexDirection: 'row',
+              paddingRight: 15,
+              alignItems: 'center',
+            }}>
+            <View style={{paddingLeft: 20, flexDirection: 'row'}}>
+              <MaterialCommunityIcons
+                name="bell-ring-outline"
+                size={30}
+                color="#868B98"
+                style={{alignSelf: 'center'}}
+              />
+              <View style={{flexDirection: 'column'}}>
+                <Text
+                  style={{
+                    fontSize: 20,
+                    fontWeight: 'bold',
+                    color: '#868B98',
+                    paddingLeft: 10,
+                  }}>
+                  Alarm Ringtone
+                </Text>
+                <Text
+                  style={{
+                    fontSize: 13,
+                    // fontWeight: 'bold',
+                    color: '#868B98',
+                    paddingLeft: 10,
+                  }}>
+                  Go to settings
+                </Text>
+              </View>
+            </View>
+            {/* rightArrow */}
+            <View>
+              <MaterialCommunityIcons
+                name="chevron-right"
+                size={30}
+                color="#868B98"
+                // style={{alignSelf: 'center'}}
+              />
+            </View>
+          </View>
+        </TouchableOpacity>
       </View>
     );
   };
