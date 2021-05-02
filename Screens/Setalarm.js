7f71ad77551a344a6398e76ac2e7bbd557fb27e0
sanimridha <sanimridha307@gmail.com>
2021-05-02 16:10:01 +0600

    set Alarm header implemented

--- Screens/Setalarm.js
+++ Screens/Setalarm.js
@@ -1,12 +1,31 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import Feather from 'react-native-vector-icons/Feather';
 
-const Setalarm = () => {
-  return (
-    <View>
-      <Text>Set Alarm Screen</Text>
-    </View>
-  );
+const Setalarm = ({navigation}) => {
+  const renderHeader = () => {
+    return (
+      <View
+        style={{
+          marginLeft: '2%',
+          height: '10%',
+          justifyContent: 'center',
+        }}>
+        <View style={{justifyContent: 'space-between', flexDirection: 'row'}}>
+          <Text style={{fontSize: 27, fontWeight: 'bold'}}>Set an alarm</Text>
+          <View style={{alignSelf: 'center', marginRight: '2%'}}>
+            <TouchableOpacity
+              activeOpacity={0.5}
+              onPress={() => navigation.navigate('Launching')}
+              style={{}}>
+              <Feather name="eye" size={25} />
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+    );
+  };
+  return renderHeader();
 };
 
 export default Setalarm;
