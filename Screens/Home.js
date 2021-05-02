ad9a6cbd604cff48928adb0df0fb7720778b6f3f
sanimridha <sanimridha307@gmail.com>
2021-05-02 13:05:42 +0600

    added checkbox

--- Screens/Home.js
+++ Screens/Home.js
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {ScrollView, StyleSheet, Text, View} from 'react-native';
+import CheckBox from 'react-native-check-box';
 
 const Home = () => {
+  const [isSelected, setSelection] = useState(false);
+
   const renderHeader = () => {
     return (
       <View
@@ -15,11 +18,46 @@ const Home = () => {
       </View>
     );
   };
+  const renderBody = () => {
+    return (
+      <View style={{height: '100%', width: '100%'}}>
+        <View
+          style={{
+            width: '100%',
+            height: '15%',
+            backgroundColor: 'white',
+            // shadowOffset: {
+            //   height: 5,
+            //   width: 3,
+            // },
+            shadowRadius: 5,
+            shadowOffset: 5,
+            shadowColor: 'black',
+          }}>
+          <View style={{width: '10%'}}>
+            <CheckBox
+              style={{padding: 10}}
+              isChecked={isSelected}
+              checkBoxColor={'#1cd2d9'}
+              onClick={() => {
+                if (isSelected == true) {
+                  setSelection(false);
+                }
+                if (isSelected == false) {
+                  setSelection(true);
+                }
+              }}
+            />
+          </View>
+        </View>
+      </View>
+    );
+  };
 
   return (
     <>
       {renderHeader()}
-      <ScrollView></ScrollView>
+      {renderBody()}
     </>
   );
 };
