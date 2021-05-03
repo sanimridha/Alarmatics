530d4514b627977dc66eb1781aeddafdd79051de
sanimridha <sanimridha307@gmail.com>
2021-05-03 11:24:33 +0600

    added setting component

---
+++ components/Settings.js
@@ -0,0 +1,212 @@
+import React from 'react';
+import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+
+const Settings = () => {
+  return (
+    <View style={{}}>
+      <TouchableOpacity
+        activeOpacity={0.8}
+        style={{
+          height: 70,
+          width: '95%',
+          backgroundColor: 'white',
+          borderRadius: 10,
+          alignSelf: 'center',
+          justifyContent: 'center',
+          marginTop: 10,
+          // _________SHADOW
+          shadowColor: '#000',
+          shadowOffset: {
+            width: 2,
+            height: 2,
+          },
+          shadowOpacity: 0.25,
+          shadowRadius: 3.84,
+          elevation: 3,
+        }}>
+        <View
+          style={{
+            justifyContent: 'space-between',
+            flexDirection: 'row',
+            paddingRight: 15,
+            alignItems: 'center',
+          }}>
+          <View style={{paddingLeft: 20, flexDirection: 'row'}}>
+            <MaterialCommunityIcons
+              name="bell-ring-outline"
+              size={30}
+              color="#868B98"
+              style={{alignSelf: 'center'}}
+            />
+            <View style={{flexDirection: 'column'}}>
+              <Text
+                style={{
+                  fontSize: 20,
+                  fontWeight: 'bold',
+                  color: '#868B98',
+                  paddingLeft: 10,
+                }}>
+                Alarm Ringtone
+              </Text>
+              <Text
+                style={{
+                  fontSize: 13,
+                  // fontWeight: 'bold',
+                  color: '#868B98',
+                  paddingLeft: 10,
+                }}>
+                Go to settings
+              </Text>
+            </View>
+          </View>
+          {/* rightArrow */}
+          <View>
+            <MaterialCommunityIcons
+              name="chevron-right"
+              size={30}
+              color="#868B98"
+              // style={{alignSelf: 'center'}}
+            />
+          </View>
+        </View>
+      </TouchableOpacity>
+      <TouchableOpacity
+        activeOpacity={0.8}
+        style={{
+          height: 70,
+          width: '95%',
+          backgroundColor: 'white',
+          borderRadius: 10,
+          alignSelf: 'center',
+          justifyContent: 'center',
+          marginTop: 10,
+          // _________SHADOW
+          shadowColor: '#000',
+          shadowOffset: {
+            width: 2,
+            height: 2,
+          },
+          shadowOpacity: 0.25,
+          shadowRadius: 3.84,
+          elevation: 3,
+        }}>
+        <View
+          style={{
+            justifyContent: 'space-between',
+            flexDirection: 'row',
+            paddingRight: 15,
+            alignItems: 'center',
+          }}>
+          <View style={{paddingLeft: 20, flexDirection: 'row'}}>
+            <MaterialCommunityIcons
+              name="bell-ring-outline"
+              size={30}
+              color="#868B98"
+              style={{alignSelf: 'center'}}
+            />
+            <View style={{flexDirection: 'column'}}>
+              <Text
+                style={{
+                  fontSize: 20,
+                  fontWeight: 'bold',
+                  color: '#868B98',
+                  paddingLeft: 10,
+                }}>
+                Alarm Ringtone
+              </Text>
+              <Text
+                style={{
+                  fontSize: 13,
+                  // fontWeight: 'bold',
+                  color: '#868B98',
+                  paddingLeft: 10,
+                }}>
+                Go to settings
+              </Text>
+            </View>
+          </View>
+          {/* rightArrow */}
+          <View>
+            <MaterialCommunityIcons
+              name="chevron-right"
+              size={30}
+              color="#868B98"
+              // style={{alignSelf: 'center'}}
+            />
+          </View>
+        </View>
+      </TouchableOpacity>
+      <TouchableOpacity
+        activeOpacity={0.8}
+        style={{
+          height: 70,
+          width: '95%',
+          backgroundColor: 'white',
+          borderRadius: 10,
+          alignSelf: 'center',
+          justifyContent: 'center',
+          marginTop: 10,
+          // _________SHADOW
+          shadowColor: '#000',
+          shadowOffset: {
+            width: 2,
+            height: 2,
+          },
+          shadowOpacity: 0.25,
+          shadowRadius: 3.84,
+          elevation: 3,
+        }}>
+        <View
+          style={{
+            justifyContent: 'space-between',
+            flexDirection: 'row',
+            paddingRight: 15,
+            alignItems: 'center',
+          }}>
+          <View style={{paddingLeft: 20, flexDirection: 'row'}}>
+            <MaterialCommunityIcons
+              name="bell-ring-outline"
+              size={30}
+              color="#868B98"
+              style={{alignSelf: 'center'}}
+            />
+            <View style={{flexDirection: 'column'}}>
+              <Text
+                style={{
+                  fontSize: 20,
+                  fontWeight: 'bold',
+                  color: '#868B98',
+                  paddingLeft: 10,
+                }}>
+                Alarm Ringtone
+              </Text>
+              <Text
+                style={{
+                  fontSize: 13,
+                  // fontWeight: 'bold',
+                  color: '#868B98',
+                  paddingLeft: 10,
+                }}>
+                Go to settings
+              </Text>
+            </View>
+          </View>
+          {/* rightArrow */}
+          <View>
+            <MaterialCommunityIcons
+              name="chevron-right"
+              size={30}
+              color="#868B98"
+              // style={{alignSelf: 'center'}}
+            />
+          </View>
+        </View>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default Settings;
+
+const styles = StyleSheet.create({});
